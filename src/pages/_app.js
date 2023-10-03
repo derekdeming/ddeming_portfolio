6a765e64f8d0035154898a4b7fbd1de7d943bacb
Derek <derekdeming17@gmail.com>
2023-10-02 20:24:44 -0600

    working front page with resume download

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import NavBar from '@/components/NavBar'
 import '@/styles/globals.css'
 import  { Montserrat } from 'next/font/google'
 import Head from 'next/head'
+import Footer from '../../Next.js portfolio website code-V2-Personal/src/components/Footer'
 
 
 
@@ -18,11 +19,10 @@ export default function App({ Component, pageProps }) {
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.ico" />
   </Head>
-
-
   <main className={'${montserrat.variable} font-mont bg-light w-full min-h-screen'}>
     <NavBar />
     <Component {...pageProps} />
+    <Footer />
   </main>
 </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,10 @@
-import AnimatedText from "@/components/AnimatedText";
-import { HireMe } from "@/components/HireMe";
+import Head from 'next/head'
+import Layout from '@/components/Layout'
 import { LinkArrow } from "@/components/Icons";
-import Layout from "@/components/Layout";
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg";
+import Image from 'next/image'
 import profilePic from "../../public/images/profile/developer-pic-1.png";
-import TransitionEffect from "@/components/TransitionEffect";
-
+import AnimatedText from '@/components/AnimatedText';
+import Link from 'next/link';
 
 
 
@@ -16,7 +12,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Awesome Portfolio Built with Nextjs</title>
+        <title>Career Portfolio</title>
         <meta
           name="description"
           content="Explore Derek Deming's Next.js developer portfolio and 
@@ -24,68 +20,41 @@ export default function Home() {
         Showcase your skills as a full-stack developer and software engineer."
         />
       </Head>
-
-      <TransitionEffect />
-      <article
-        className={`flex min-h-screen items-center text-dark dark:text-light sm:items-start`}
-      >
-        <Layout className="!pt-0 md:!pt-16 sm:!pt-16">
-          <div className="flex w-full items-start justify-between md:flex-col">
-            <div className="w-1/2 lg:hidden md:inline-block md:w-full">
-              <Image
-                src={profilePic}
-                alt="DerekDeming"
-                className="h-auto w-full"
-                sizes="100vw"
-                priority
-              />
-            </div>
-            <div className="flex w-1/2 flex-col items-center self-center lg:w-full lg:text-center">
-              <AnimatedText
-                text="Turning vision into reality with code and automation."
-                className="!text-left !text-6xl xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
-              />
-              <p className="my-4 text-base font-medium md:text-sm sm:!text-xs">
-              As a skilled full-stack ML Engineer, I am dedicated to turning ideas into innovative automated solutions. Explore my latest projects and articles, showcasing my expertise in full stack engineering specializing in ML and LLM integration.
-              </p>
-              <div className="mt-2 flex items-center self-start lg:self-center">
+      <main className='flex items-center text-dark w-full min-h-screen'> 
+        <Layout className='pt-0'>
+            <div className='flex items-center justify-between w-full'>
+              <div className='w-1/2'>
+                  <Image                 
+                    src={profilePic}
+                    alt="DerekDeming"
+                    className="w-full h-auto"
+                    sizes="100vw"
+                    priority
+                  />
+              </div>
+              <div className='w-1/2 flex flex-col items-center self-center'>
+                <AnimatedText text="Turning Vision Into Reality With Code and Automation." className="!text-6xl !text-left" />
+                <p className='my-4 text-base font-medium'>
+                As a skilled full-stack ML Engineer, I am dedicated to turning ideas into innovative automated solutions. Explore my latest projects and articles, showcasing my expertise in full stack engineering specializing in ML and LLM integration.
+                </p>
+              <div>
                 <Link
-                  // whileHover={{
-                  //   cursor: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='40' height='48' viewport='0 0 100 100' style='font-size:24px;'><text y='50%'>👆</text></svg>"), auto`,
-                  // }}
-                  href="/All-Texts/Derek-Deming-Resume.pdf"
-                  target={"_blank"}
-                  className={`flex items-center rounded-lg border-2 border-solid bg-dark p-2.5 px-6 text-lg font-semibold
-            capitalize text-light hover:border-dark hover:bg-transparent hover:text-dark 
-            dark:bg-light dark:text-dark dark:hover:border-light dark:hover:bg-dark dark:hover:text-light
-            md:p-2 md:px-4 md:text-base
-             `}
-                  download
-                >
-                  Resume <LinkArrow className="ml-1 !w-6 md:!w-4" />
-                </Link>
-
-                <Link
-                  href="mailto:derekdeming17@gmail.com"
-                  className="ml-4 text-lg font-medium capitalize text-dark underline 
-                  dark:text-light md:text-base"
-                >
-                  Contact
-                </Link>
+                    // whileHover={{
+                    //   cursor: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='40' height='48' viewport='0 0 100 100' style='font-size:24px;'><text y='50%'>👆</text></svg>"), auto`,
+                    // }}
+                    href="/All-Texts/Derek-Deming-Resume.pdf"
+                    target={"_blank"}
+                    className={`flex items-center rounded-lg border-2 border-solid bg-dark p-2.5 px-6 text-lg font-semibold
+              capitalize text-light hover:border-dark hover:bg-transparent hover:text-dark 
+              dark:bg-light dark:text-dark dark:hover:border-light dark:hover:bg-dark dark:hover:text-light
+              md:p-2 md:px-4 md:text-base`}
+                    download> Resume <LinkArrow className="ml-1 !w-6 md:!w-4" />
+              </Link>
               </div>
             </div>
           </div>
         </Layout>
-
-        <HireMe />
-        <div className="absolute right-8 bottom-8 inline-block w-24 md:hidden">
-          <Image
-            className="relative h-auto w-full"
-            src={lightBulb}
-            alt="Codebucks"
-          />
-        </div>
-      </article>
+      </main>
     </>
-  );
+  )
 }
