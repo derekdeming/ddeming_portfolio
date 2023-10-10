45ac8166ab0fe67c8e57b9f2a6d656c7946582ba
Derek <derekdeming17@gmail.com>
2023-10-10 16:45:58 -0600

    education added

--- src/components/hooks/useThemeSwitcher.js
+++ src/components/hooks/useThemeSwitcher.js
@@ -1,58 +1,57 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from "react";
 
-const useThemeSwitcher = () => {
+export function useThemeSwitch() {
+  const preferDarkQuery = "(prefers-color-scheme: dark)";
+  const [mode, setMode] = useState("");
 
-    const preferDarkQuery = "(prefer-color-scheme: dark)";
-    const [mode, setMode] = useState("");
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(preferDarkQuery);
+    const userPref = window.localStorage.getItem("theme");
 
-    useEffect(() => {
-
-        const mediaQuery = window.matchMedia(preferDarkQuery)
-        const userPref = window.localStorage.getItem("theme")
-
-        const handleChange = () => {
-            if(userPref){
-                let check = userPref === "dark" ? "dark" : "light";
-                setMode(check)
-                if(check==="dark"){
-                    document.documentElement.classList.add("dark")
-                }else{
-                    document.documentElement.classList.remove("dark")
-                }
-            }else{
-                let check = mediaQuery.matches ? "dark" : "light";
-                setMode(check);
-                
-                if(check==="dark"){
-                    document.documentElement.classList.add('dark')
-                }else{
-                    document.documentElement.classList.remove('dark');
-                }
-            }
+    const handleChange = () => {
+      if (userPref) {
+        let check = userPref === "dark" ? "dark" : "light";
+        setMode(check);
+        if (check === "dark") {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
         }
-        mediaQuery.addEventListener("change", handleChange)
-
-        return () => mediaQuery.removeEventListener("change", handleChange)
-
-
-    }, [])
-
-    useEffect(() => {
-        if(mode==="dark"){
-            window.localStorage.setItem("theme", "dark");
-            document.documentElement.classList.add("dark")
-        }else{
-            window.localStorage.setItem("theme", "light");
-            document.documentElement.classList.remove("dark")
+        return check;
+      } else {
+        setMode(mediaQuery.matches ? "dark" : "light");
+        window.localStorage.setItem(
+          "theme",
+          mediaQuery.matches ? "dark" : "light"
+        );
+        if (mediaQuery.matches) {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
         }
+        return window.matchMedia(preferDarkQuery).matches ? "dark" : "light";
+      }
+    };
 
-    }, [mode])
+    handleChange();
 
+    mediaQuery.addEventListener("change", handleChange);
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
 
+  useEffect(() => {
+    if (mode === "dark") {
+      document.documentElement.classList.add("dark");
+      window.localStorage.setItem("theme", "dark");
+    }
 
-  return (
-    [mode, setMode]
-  )
+    if (mode === "light") {
+      document.documentElement.classList.remove("dark");
+      window.localStorage.setItem("theme", "light");
+    }
+  }, [mode]);
+
+  // we're doing it this way instead of as an effect so we only
+  // set the localStorage value if they explicitly change the default
+  return [mode, setMode];
 }
-
-export default useThemeSwitcher
